27c07356308e8086c6a6b1f373d27b9677d4548d
iTzMeRafa <rafael97@gmx.de>
2019-01-06 23:10:15 +0100

    added 3 decimal places

--- game/server/entities/character.cpp
+++ game/server/entities/character.cpp
@@ -1475,8 +1475,8 @@ void CCharacter::HandleTiles(int Index, float FractionOfTick)
 	}
 
 	// finish
-	if(((m_TileIndex == TILE_END) || (m_TileFIndex == TILE_END) || FTile1 == TILE_END || FTile2 == TILE_END || FTile3 == TILE_END || FTile4 == TILE_END || Tile1 == TILE_END || Tile2 == TILE_END || Tile3 == TILE_END || Tile4 == TILE_END) && m_DDRaceState == DDRACE_STARTED)
-		Controller->m_Teams.OnCharacterFinish(m_pPlayer->GetCID(), FractionOfTick);
+	if(((m_TileIndex == TILE_END) || (m_TileFIndex == TILE_END)) && m_DDRaceState == DDRACE_STARTED)
+    		Controller->m_Teams.OnCharacterFinish(m_pPlayer->GetCID(), FractionOfTick);
 
 	// freeze
 	if(((m_TileIndex == TILE_FREEZE) || (m_TileFIndex == TILE_FREEZE)) && !m_Super && !m_DeepFreeze)
