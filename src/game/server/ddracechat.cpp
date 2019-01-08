395d0b3a33fea5a2cfecf502c07f8323316c49da
iTzMeRafa <rafael97@gmx.de>
2019-01-08 17:23:30 +0100

    qwe

--- src/game/server/ddracechat.cpp
+++ src/game/server/ddracechat.cpp
@@ -647,17 +647,10 @@ void CGameContext::ConLoad(IConsole::IResult *pResult, void *pUserData)
 			return;
 #endif
 
-#if defined(CONF_SQL)
-	if(g_Config.m_SvUseSQL)
-		pPlayer->m_LastSQLQuery = pSelf->Server()->Tick();
-#endif
-}
-
-#if defined(CONF_SQL)
-	if(g_Config.m_SvUseSQL)
-		if(pPlayer->m_LastSQLQuery + g_Config.m_SvSqlQueriesDelay * pSelf->Server()->TickSpeed() >= pSelf->Server()->Tick())
-			return;
-#endif
+	if (pResult->NumArguments() > 0)
+		pSelf->Score()->LoadTeam(pResult->GetString(0), pResult->m_ClientID);
+	else
+		return;
 
 #if defined(CONF_SQL)
 	if(g_Config.m_SvUseSQL)
